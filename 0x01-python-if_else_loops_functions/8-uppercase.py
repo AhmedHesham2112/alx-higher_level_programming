d80f452a82f6480af335b1b7b8bf98e99df364f2
root <root@DESKTOP-1N3TEFC>
2024-01-02 22:47:38 +0200

    10-add.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
-def uppercase(s):
-    for c in s:
+def uppercase(str):
+    for c in str:
         if (ord(c) >= ord('a')) and (ord(c) <= ord('z')):
             c = chr(ord(c)-ord('a')+ord('A'))
         print("{}".format(c), end='')
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -10,4 +10,4 @@ def print_last_digit(number):
         while number >= 10:
             number = number % 10
     print("{}".format(number), end="")
-    return (number)
+    return number
