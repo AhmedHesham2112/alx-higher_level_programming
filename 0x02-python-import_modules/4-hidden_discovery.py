90e067a89e4e8c9fecd1a962e4f98cef1a41cff6
root <root@DESKTOP-1N3TEFC>
2024-01-05 02:48:43 +0200

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,8 @@
 
 import hidden_4
 
-name = dir(hidden_4)
-for i in range(len(name)):
-    if name[i][:2] != "__":
-        print(name[i])
+if __name__ == "__main__":
+    name = dir(hidden_4)
+    for i in range(len(name)):
+        if name[i][:2] != "__":
+            print(name[i])
