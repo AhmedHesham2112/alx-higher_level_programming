13f11255d261a953859b7ae24748c3f6245b877e
root <root@DESKTOP-1N3TEFC>
2024-01-06 20:50:04 +0200

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     if len == 0:
         tuple_0 = (0, "None")
-        return tuple_0
     else:
         tuple_0 = (length, sentence[0])
-        return tuple_0
+    return tuple_0
