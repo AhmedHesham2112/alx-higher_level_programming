c92dae6798a2b0b131b1d094474237394082d57d
root <root@DESKTOP-1N3TEFC>
2024-01-23 21:47:32 +0200

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,12 +14,19 @@ class Square():
 
         Args:
             size (int): the size of the new square.
+            position (int, int): the position in the square
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        if not (isinstance(position, tuple) or
+                len(position) != 2 or
+                not all(isinstance(num, int) for num in position) or
+                not all(num > 0 for num in position)):
+            raise TypeError("sposition must be a tuple of 2 positive integers")
         self.__size = size
+        self.position = position
 
     def area(self):
         """
@@ -48,7 +55,28 @@ class Square():
         """
         if self.__size == 0:
             print()
+            return
+        for i in range(self.__position[1]):
+            print()
         for i in range(0, self.__size):
+            for k in range(0, self.__position[0]):
+                print(" ", end="")
             for j in range(0, self.__size):
                 print("#", end="")
             print()
+
+    @property
+    def position(self):
+        """
+        Get the current position of the square.
+        """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
