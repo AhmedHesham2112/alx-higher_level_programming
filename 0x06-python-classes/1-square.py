bfa7f23932dcb0e764d6351c8702270987208ef2
root <root@DESKTOP-1N3TEFC>
2024-01-23 20:38:03 +0200

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Define a class Square.
+"""
+
+
+class Square():
+    """
+    Represent a Square
+    """
+    def __init__(self, size):
+        """
+        initialize a new Square
+
+        Args:
+            size (int): the size of the new square.
+        """
+        self.__size = size
