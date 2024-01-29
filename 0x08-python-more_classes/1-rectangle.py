58427afcf19ed28336d647314299a09768f985cd
root <root@DESKTOP-1N3TEFC>
2024-01-29 19:20:26 +0200

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle():
             width (int): width of the square
             hegiht (int): heghit of the square
         """
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
 
     @property
     def height(self):
@@ -25,6 +25,10 @@ class Rectangle():
 
     @height.setter
     def height(self, value):
+        if not isinstance(value,int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @property
@@ -36,4 +40,8 @@ class Rectangle():
 
     @width.setter
     def width(self, value):
+        if not isinstance(value,int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
