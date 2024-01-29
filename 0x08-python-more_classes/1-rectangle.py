cf6374bdddce54eeaa95c924f7f7752790d7a9da
root <root@DESKTOP-1N3TEFC>
2024-01-29 19:14:54 +0200

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class"""
+
+
+class Rectangle():
+    """"Represent a Rectangle"""
+    pass
+
+    def __init__(self, width=0, height=0):
+        """
+        initialized the width and height with 0, 0 or as given
+        Args:
+            width (int): width of the square
+            hegiht (int): heghit of the square
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """
+        Get the current height of the rectangle.
+        """
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def width(self):
+        """
+        Get the current width of the rectangle.
+        """
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+    
