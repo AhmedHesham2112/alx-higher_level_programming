bff00852dad4317f300192cf405bcee7d37ee735
root <root@DESKTOP-1N3TEFC>
2024-02-05 20:13:00 +0200

    6-hash_table_delete.c

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+lookup returns the list of available attributes and methods of an object
+"""
+
+
 def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
     return dir(obj)
