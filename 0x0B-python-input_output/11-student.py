ed99193ca09d8697a3f8031d1346211fc5e93499
root <root@DESKTOP-1N3TEFC>
2024-02-06 22:59:32 +0200

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Define a Student Class and to_json function
+"""
+
+
+class Student:
+    """
+    Representation of Student Class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializing a student
+
+        Args:
+        first_name (string): the student's first name
+        last_name (string): the student's last name
+        age (int): the student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            dic = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    dic[att] = self.__dict__[att]
+            return dic
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
