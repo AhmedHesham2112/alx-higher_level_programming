7677fbbb4249b4246a856b1c70f1de89d78bdba1
root <root@DESKTOP-1N3TEFC>
2024-02-10 22:36:52 +0200

    1. Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define Base Class"""
+
+
+class Base:
+    """
+    Class Base
+
+    Attributes:
+        nb_objects (int): Private class attribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
