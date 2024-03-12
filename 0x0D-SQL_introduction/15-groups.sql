18410826ffef65610cf2d66c2c9705332f5db64d
root <root@DESKTOP-1N3TEFC>
2024-03-13 00:12:54 +0200

    15-groups.sql

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,8 @@
+-- script that lists the number of records with the same score
+-- The result should display:
+-- the score
+-- the number of records for this score with the label number
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
