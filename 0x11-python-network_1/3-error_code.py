e3f3f79768af0340d721c4fea325c82a96705eb2
root <root@DESKTOP-1N3TEFC>
2024-04-28 18:48:22 +0300

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays the
-value of the X-Request-Id variable found in the header of the response."""
+"""that takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8)."""
 
 if __name__ == '__main__':
     import urllib.request
