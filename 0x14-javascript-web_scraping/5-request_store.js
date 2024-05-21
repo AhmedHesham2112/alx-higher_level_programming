2cc7392c5d13aa66dcb7df8d02efce957ad31698
root <root@DESKTOP-1N3TEFC>
2024-05-21 20:23:14 +0300

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+request.get(process.argv[2], (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
