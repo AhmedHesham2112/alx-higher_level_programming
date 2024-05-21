3f1cd3b4ee9a1b5c6da714e22e06eb498b58640e
root <root@DESKTOP-1N3TEFC>
2024-05-21 18:03:17 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  if (error) {
-    console.log(error);
+fs.readFile(process.argv[2], 'utf8', (err, content) => {
+  if (err) {
+    console.log(err);
   } else {
     console.log(content);
   }
