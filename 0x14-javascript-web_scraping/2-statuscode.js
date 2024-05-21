0d37d21ef1d822cb1db6a54cad58d6c485d237fe
root <root@DESKTOP-1N3TEFC>
2024-05-21 18:12:21 +0300

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const request = require('request');
 request.get(process.argv[2], (err, res, body) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(`code: ${res.statusCode}`);
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`code: ${res.statusCode}`);
 });
