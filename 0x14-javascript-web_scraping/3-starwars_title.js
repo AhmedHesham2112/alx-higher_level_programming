8121e92bfccb8f7d4c0151b275fdd60d367ac929
root <root@DESKTOP-1N3TEFC>
2024-05-21 18:24:38 +0300

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request.get(url, (err, res, body) => {
   if (err) {
     return console.log(err);
