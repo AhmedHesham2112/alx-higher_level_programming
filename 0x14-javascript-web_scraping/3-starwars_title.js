8734f7ef7a3743e299f9d2e3a48994148457b364
root <root@DESKTOP-1N3TEFC>
2024-05-21 18:23:44 +0300

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`
+request.get(url, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  } else {
+    const resJSON = JSON.parse(body);
+    console.log(resJSON.title);
+  }
+});
